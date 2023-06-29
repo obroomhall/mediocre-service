8ef5d32eccc4e28294718aedb2cfe4bda31d01e8
Oliver Broomhall <obroomhall@gmail.com>
2023-06-29 23:43:00 +0100

    Turn server functions into a Server class

--- libmediocre/server/server.hpp
+++ libmediocre/server/server.hpp
@@ -2,10 +2,24 @@
 #define mediocre_server_h
 
 #include <cstdint>
+#include <grpcpp/server_builder.h>
+#include <iomanip>
 
 namespace mediocre::server {
 
-    void run_server(uint16_t port);
+    class Server {
+    private:
+        static std::string get_server_address(uint16_t port);
+        static void register_listener(grpc::ServerBuilder &builder, const std::string &server_address);
+        static void register_services(grpc::ServerBuilder &builder);
+        std::string server_address;
+        std::unique_ptr<grpc::Server> server;
+
+    public:
+        explicit Server(uint16_t port);
+        void run_server();
+    };
+
 
 }// namespace mediocre::server
 
