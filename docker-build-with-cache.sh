42635a261c412542590085fdadea553ac63cb3a2
Oliver Broomhall <obroomhall@gmail.com>
2023-07-08 00:21:48 +0100

    Update docker-build-with-cache.sh to use sha and proper ordering

--- docker-build-with-cache.sh
+++ docker-build-with-cache.sh
@@ -3,15 +3,16 @@
 
 repo='obroomhall/mediocre'
 branch=$(git branch --show-current)
+sha=$(git rev-parse --short HEAD)
 
 if [[ $branch != 'master' ]]; then
     pr=$(curl -s https://api.github.com/repos/${repo}/pulls | jq ".[] | select(.head.ref == \"$branch\") | .number" | head -1)
     docker buildx build \
             --force-rm \
             --progress plain \
-            --cache-from type=registry,ref=${repo}:master-cache \
-            --cache-from type=registry,ref=${repo}:${branch}-cache \
+            --cache-from type=registry,ref=${repo}:sha-${sha}-cache \
             --cache-from type=registry,ref=${repo}:pr-${pr}-cache \
+            --cache-from type=registry,ref=${repo}:master-cache \
             --target builder \
             -t mediocre:local-builder .
 else
