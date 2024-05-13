5273b1bd8dba2e874db8d28f1dbd0fe7eab65698
Oliver Broomhall <obroomhall@gmail.com>
2024-05-13 14:24:34 +0100

    Update commands.sh to only run the proxy (run-proxy.sh)

--- scripts/run-proxy.sh
+++ scripts/run-proxy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Starting mediocre"
-mediocre &
-
 TLS_CERT_PATH=${TLS_CERT_PATH:-/certificates/cert.pem}
 TLS_KEY_PATH=${TLS_KEY_PATH:-/certificates/key.pem}
 if [[ -f $TLS_CERT_PATH && -f $TLS_KEY_PATH ]]; then
@@ -10,7 +7,7 @@ if [[ -f $TLS_CERT_PATH && -f $TLS_KEY_PATH ]]; then
   if [ ! -z "$ALLOWED_ORIGINS" ]; then
     echo "Using allowed origins: $ALLOWED_ORIGINS"
   else
-    ALLOWED_ORIGINS=https://localhost:5173
+    ALLOWED_ORIGINS=http://localhost:5173,https://localhost:5173
     echo "No allowed origins set, falling back to $ALLOWED_ORIGINS"
   fi
 
@@ -19,9 +16,7 @@ if [[ -f $TLS_CERT_PATH && -f $TLS_KEY_PATH ]]; then
       --server_tls_cert_file=$TLS_CERT_PATH \
       --server_tls_key_file=$TLS_KEY_PATH \
       --allowed_origins=$ALLOWED_ORIGINS \
-      --backend_addr=0.0.0.0:8081 \
-      --enable_health_check_service \
-      --enable_health_endpoint
+      --backend_addr=0.0.0.0:8081
 
 else
 
@@ -35,9 +30,7 @@ else
   echo "No certificates found, starting proxy without TLS"
   grpcwebproxy \
       --run_tls_server=false \
-      --allowed_origins=ALLOWED_ORIGINS \
-      --backend_addr=0.0.0.0:8081 \
-      --enable_health_check_service \
-      --enable_health_endpoint
+      --allowed_origins=$ALLOWED_ORIGINS \
+      --backend_addr=0.0.0.0:8081
 
 fi
