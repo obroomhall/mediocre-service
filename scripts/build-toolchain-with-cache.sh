2511794ba5be91cbd17635ada7b240f375fdc910
Oliver Broomhall <obroomhall@gmail.com>
2024-09-23 10:16:09 +0100

    Fix repo name in script

--- scripts/build-toolchain-with-cache.sh
+++ scripts/build-toolchain-with-cache.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 # Builds the library using registry cache
 
-repo='obroomhall/mediocre'
+github_repo='obroomhall/mediocre-service'
+dockerhub_repo='obroomhall/mediocre'
 branch=$(git branch --show-current)
 
 cache_tags=()
+cache_tag_prefix="--cache-from type=registry,ref=${dockerhub_repo}"
 
 if [[ $branch != 'master' ]]; then
   # commit sha cache
   sha=$(git rev-parse --short HEAD)
   if [[ $sha ]]; then
-    cache_tags+=(
-      "--cache-from type=registry,ref=${repo}:sha-${sha}-develop-cache"
-    )
+    cache_tags+=("${cache_tag_prefix}:sha-${sha}-develop-cache")
   fi
 
   # pr cache
-  pr=$(curl -s https://api.github.com/repos/${repo}/pulls | jq ".[] | select(.head.ref == \"$branch\") | .number" | head -1)
+  pr=$(curl -s https://api.github.com/repos/${github_repo}/pulls | jq ".[] | select(.head.ref == \"$branch\") | .number" | head -1)
   if [[ $pr ]]; then
-    cache_tags+=(
-      "--cache-from type=registry,ref=${repo}:pr-${pr}-develop-cache"
-    )
+    cache_tags+=("${cache_tag_prefix}:pr-${pr}-develop-cache")
   fi
 fi
 
-cache_tags+=(
-  "--cache-from type=registry,ref=${repo}:master-develop-cache"
-)
+cache_tags+=("${cache_tag_prefix}:master-develop-cache")
 
 docker buildx build \
   --force-rm \
